421e364c7bd8abe3a06d9b3dd7172f52c6fc1be2
MaxKoval6 <mkoval06@gmail.com>
2022-12-30 21:54:09 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+//Task 1
+// function hello1() {
+//   const h = "Hello";
+//   const j = "JavaScript";
+//   console.log(h + " " + j);
+// }
+// hello1();
+// //Task 2
+// function hello2() {
+//   const h = "Hello";
+//   const name = "Василь";
+//   console.log(h + " " + name);
+// }
+// hello2();
+// //Task 3
+// //1
+// // const mul = function (n, m) {
+// //   return n + m;
+// // };
+
+// // const result = mul( 2, 14);
+// // console.log(result);
+// //2
+// // const mul = function (n, m) {
+// //   return n - m;
+// // };
+
+// // const result = mul(2, 14);
+// // console.log(result);
+// //3
+// const mul = function (n, m) {
+//   return n * m;
+// };
+
+// const result = mul(2, 14);
+// console.log(result);
+// //Task 4
+// const myNewArrowFunction = function (...args) {
+//   console.log(args);
+// };
+
+// myNewArrowFunction(1, 2, 3);
+// myNewArrowFunction(100, 200, 300, 400, 500);
+// myNewArrowFunction("Js", "Python", "Java", "PHP", "C++");
+//Task 5
+// const myAverageScore = function (array) {
+//   let result = 0;
+//   for (const number of array) {
+//     result = (result + number) / array.length;
+//   }
+//   if (result > 91 && result < 100) {
+//     console.log("A");
+//   }
+//   if (result > 81 && result < 90) {
+//     console.log("B");
+//   }
+//   if (result > 71 && result < 80) {
+//     console.log("C");
+//   }
+//   if ((result) => 0 && result < 70) {
+//     console.log("D");
+//   }
+// };
+
+//console.log(myAverageScore([100, 95, 89, 96]));
+//console.log(myAverageScore([45, 63, 85, 70]));
+//console.log(myAverageScore([77, 82, 60, 58]));
+//console.log(myAverageScore([93, 99, 93, 96]));
